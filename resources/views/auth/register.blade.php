08387ab968cf612bc8f02e4cec67714fba4a4dc8
Al-Amin-Ceazer <ceazer.startern@gmail.com>
2019-12-04 18:13:05 +0600

    add javascript validation on user register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,28 +8,16 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" id="userRegistrationForm">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +31,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -57,7 +45,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
                             </div>
                         </div>
 
@@ -74,4 +62,46 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('page-js')
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script>
+
+        document.querySelector('#userRegistrationForm').addEventListener('submit', function (e) {
+          alert(111);
+          e.preventDefault();
+          axios.post(this.action, {
+            'email': document.querySelector('#email').value,
+            'password': document.querySelector('#password').value,
+            'password_confirmation': document.querySelector('#password-confirm').value
+          })
+          .then((response) => {
+            clearErrors();
+            this.submit();
+          })
+          .catch((error) => {
+            const errors = error.response.data.errors
+            const firstItem = Object.keys(errors)[0]
+            const firstItemDOM = document.getElementById(firstItem)
+            const firstErrorMessage = errors[firstItem][0]
+            // scroll to the error message
+            firstItemDOM.scrollIntoView()
+            clearErrors()
+            // show the error message
+            firstItemDOM.insertAdjacentHTML('afterend', `<div class="text-danger">${firstErrorMessage}</div>`)
+            // highlight the form control with the error
+            firstItemDOM.classList.add('border', 'border-danger')
+          });
+        });
+        function clearErrors() {
+          // remove all error messages
+          const errorMessages = document.querySelectorAll('.text-danger')
+          errorMessages.forEach((element) => element.textContent = '')
+          // remove all form controls with highlighted error text box
+          const formControls = document.querySelectorAll('.form-control')
+          formControls.forEach((element) => element.classList.remove('border', 'border-danger'))
+        }
+
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -51,7 +51,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->email }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -76,5 +76,7 @@
             @yield('content')
         </main>
     </div>
+    {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
+    @yield('page-js')
 </body>
 </html>
